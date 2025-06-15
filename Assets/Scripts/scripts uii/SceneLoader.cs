353926d7c51fbbee62a20f731960ebbd834cfbf8
Aicha295 <107948361+Aicha295@users.noreply.github.com>
2025-06-16 02:06:03 +0300

    رابط ui ب games  بارب نخلص بقا

---
+++ Assets/Scripts/scripts uii/SceneLoader.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    //---------------------------------------------
+    // Function to Load Scene 0
+    public void LoadSceneIOSGame()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    //---------------------------------------------
+    // Function to Load Scene 1
+    public void LoadSceneIOSInfo()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+    public void BackFromIOSGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+
+    //---------------------------------------------
+    // Function to Load Scene 2
+    public void LoadSceneIOTGame()
+    {
+        SceneManager.LoadScene(8);
+    }
+
+    //---------------------------------------------
+    // Function to Load Scene 3
+    public void LoadSceneIOTInfo()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+
+    public void BackFromIOTGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+
+
+    //---------------------------------------------
+    // Function to Load Scene 4
+    public void LoadSceneXRGame()
+    {
+        SceneManager.LoadScene(7);
+    }
+    public void BackFromXRGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+
+
+    //---------------------------------------------
+    // Function to Load Scene 5
+    public void LoadSceneXRInfo()
+    {
+        SceneManager.LoadScene(16);
+    }
+
+    //---------------------------------------------
+    // Function to Load Scene 6
+    public void LoadSceneEmbeddedGame()
+    {
+        SceneManager.LoadScene(9);
+    }
+
+    //---------------------------------------------
+    // Function to Load Scene 7
+    public void LoadSceneEmbeddedInfo()
+    {
+        SceneManager.LoadScene(7);
+    }
+    public void BackFromEmbeddedGame()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    //---------------------------------------------
+    // Function to Load Scene 8
+    public void LoadSceneGalleryGame()
+    {
+        SceneManager.LoadScene(8);
+    }
+
+    //---------------------------------------------
+    // Function to Load Scene 9
+    public void LoadSceneGalleryInfo()
+    {
+        SceneManager.LoadScene(9);
+    }
+}
