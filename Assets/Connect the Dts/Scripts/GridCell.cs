881bfa7366b746ad010653c0815dd11312a3e945
MariaNady21 <mariaanadysalama35_sd@fcis.bsu.edu.eg>
2025-06-16 01:26:22 +0300

    iot

---
+++ Assets/Connect the Dts/Scripts/GridCell.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class GridCell : MonoBehaviour
+{
+    public Vector2Int GridPosition;
+    public Vector3 GetCenterPosition()
+    {
+        return transform.position;
+    }
+}
