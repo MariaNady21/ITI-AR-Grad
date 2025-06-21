fec3e7fbf55943ad6a7ab6467a6107863e53e00d
Maryam Mahmoud Anwar Radwan <99343652+Mariemanwer@users.noreply.github.com>
2025-06-22 00:33:36 +0200

    ياااارب نخلص و نتخرج

--- Assets/General UI/Scripts/LoadGameSceneFromInfo.cs
+++ Assets/General UI/Scripts/LoadGameSceneFromInfo.cs
@@ -3,9 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class LoadGameSceneFromInfo : MonoBehaviour
 {
-    public void LoadScene()
+    public void LoadSceneGame()
     {
         SceneEntryMode.isInfoMode = true;
         SceneManager.LoadScene(3);
+    }
+
+    public void LoadSceneGallery()
+    {
+        SceneEntryMode.isInfoMode = true;
+        SceneManager.LoadScene(5);
+    }
+
+    public void LoadSceneIOS()
+    {
+        SceneEntryMode.isInfoMode = true;
+        SceneManager.LoadScene(1);
     }
 }
